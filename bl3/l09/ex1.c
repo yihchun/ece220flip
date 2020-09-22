eb34d272a910eea04aa06405db8dca9c40cdd991
Yih-Chun Hu <yihchun@somedomain.com>
2020-09-22 23:36:15 +0800

    bl3

--- bl3/l09/ex1.c
+++ bl3/l09/ex1.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-int maxk(int n){
-    /* Your code here */
 
+int maxk(int n){
+    /*
+    if (n == 1) return 0;
+    return 1+maxk(n/2);
+    */
+    int i;
+    for (i=0; (1<<i)<=n; i++)
+	;
+    return i-1;
 }
 
 int main(){
--- bl3/l09/ex2.c
+++ bl3/l09/ex2.c
@@ -2,14 +2,35 @@
 #include <math.h>
 /* Calculate the number of digits in base N */
 int getDigitBaseN(int number, int N){
-    /* Your code here */
-
+    /*
+    if (number == 0) return 0;
+    return 1+getDigitBaseN(number/N, N);
+    */
+    int ret = 0;
+    while (number) {
+	ret++;
+	number /= N;
+    }
+    return ret;
 }
 /* Print out a number in base N */
 void printBaseN(int number, int N){
     int digit = getDigitBaseN(number, N);
-    /* Your code here */
- 
+    int i, j;
+    int cur, res;
+    for (i=digit; i; i--) { /* loop through each digit to print */
+	cur = 1; /* N^(i-1) */
+	res = 0; /* number / cur */
+	for (j=0; j<i-1; j++) /* computes the N^(i-1) iteratively */
+	    cur *= N;
+	while (number >= cur) { /* computes number / cur by repeated sub */
+	    number -= cur;
+	    res++;
+	}
+	/* above loop has the side effect of setting number = number % cur */
+	printf("%d", res); /* prints number/cur, which is current digit */
+    }
+    printf("\n");
 }
 int main(){
     int number, N;
